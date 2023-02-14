e42d551bd034a32d95c38e88bcd0ff0992bdf561
João Felipe <coleojones@gmail.com>
2023-02-14 18:30:53 -0300

    New commit

---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character{Id = 1, Name = "Sam"}
+        };
+        public async Task<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return characters;
+        }
+
+        public async Task<List<Character>> GetAllCharacters()
+        {
+            return characters;
+        }
+
+        public async Task<Character> GetCharacterById(int id)
+        {
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            if (character is not null)
+                return character;
+
+            throw new Exception("Character not found");
+        }
+    }
+}
