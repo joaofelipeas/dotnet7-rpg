4a684326710c389aa60932a0e88fd27cacb7e0c0
João Felipe <coleojones@gmail.com>
2023-02-15 18:09:28 -0300

    Web API Summary Entity Framework 7 Introduction Object-Relational-Mapping & Code-First Migration Explained Installing Entity Framework 7 Installing SQL Server Express (with Management Studio) Implementing the DataContext ConnectionString & Adding the DbContext

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 global using dotnet_rpg.Models;
 global using dotnet_rpg.Services.CharacterService;
 global using dotnet_rpg.Dto.Character;
+global using Microsoft.EntityFrameworkCore;
+global using dotnet_rpg.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
